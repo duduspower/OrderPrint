b12f091f083ed2be6d6f83b7576958d432c5be27
dominikduda191 <dominikduda191@gmail.com>
2022-04-24 21:37:17 +0200

    First commit!

---
+++ src/main/java/com/example/print/OrderPrintApplication.java
@@ -0,0 +1,13 @@
+package com.example.print;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderPrintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderPrintApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/print/OrderPrintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.print;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderPrintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
